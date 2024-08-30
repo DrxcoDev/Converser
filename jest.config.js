9cb6d81602df6509f219c52114077e3a304e1e11
Drxco <156522801+DrxcoDev@users.noreply.github.com>
2024-08-30 17:38:06 +0000

    Commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    transform: {
+      "^.+\\.ts$": "ts-jest"
+    },
+    testEnvironment: "node",
+    moduleFileExtensions: ["ts", "js"],
+    testMatch: ["**/tests/**/*.test.ts"]
+  };
+  
